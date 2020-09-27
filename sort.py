854d57802ab5ab9c9b5bce072a81915e9a12dc81
TGontar <gontar.ts@mail.ru>
2020-09-27 22:42:53 +0300

    kostya

--- sort.py
+++ sort.py
@@ -19,3 +19,36 @@ def cocktail(a):
         if not swap:
             break
 
+#КОД ТИМУРА
+def mergeSort(alist):
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i<len(lefthalf) and j<len(righthalf):
+            if lefthalf[i]<righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i<len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j<len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    return ' '.join(map(str, alist))
+#Здесь был костя
